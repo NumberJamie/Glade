3da3a082586f53bd1daa34bf00379bed40c98b4d
NumberJamie <jamievanessen@gmail.com>
2025-04-20 10:11:05 +0200

    Initial commit

---
+++ src/main/java/com/nrjam/glade/Glade.java
@@ -0,0 +1,16 @@
+package com.nrjam.glade;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Glade implements ModInitializer {
+	public static final String MOD_ID = "glade";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
